57722db9a8da3a231a697d6cc3dfdaa2b316d15b
Pablo <pablopadulles@gmail.com>
2023-10-01 22:58:34 -0300

    add year y student field in payment

--- payment.py
+++ payment.py
@@ -26,11 +26,41 @@ class SchoolPayment(ModelSQL, ModelView):
         ('paid', 'Paid'),
         ('must', 'Must')], 'State', readonly=True)
     amount_paid = fields.Numeric('Amount Paid')
+    student = fields.Function(fields.Many2One('party.party', 'Student'), 'get_studet',
+                              searcher='search_student')
+    year = fields.Function(fields.Integer('Year'), 'get_year', searcher='search_year')
+    currency = fields.Many2One('currency.currency', 'Currency')
 
     @staticmethod
     def default_state():
         return 'must'
     
+    @staticmethod
+    def default_currency():
+        Company = Pool().get('company.company')
+        if Transaction().context.get('company'):
+            company = Company(Transaction().context['company'])
+            return company.currency.id
+
+    def get_studet(self, name):
+        if self.inscription:
+            return self.inscription.student.id
+    
+    def get_year(self, name):
+        if self.inscription:
+            return self.inscription.year
+    
+    @classmethod
+    def search_student(cls, name, clause):
+        return [('inscription.student',) + tuple(clause[1:])]
+    
+    @classmethod
+    def search_year(cls, name, clause):
+        return [('inscription.year',) + tuple(clause[1:])]
+
+    
 
 class SchoolPaymentReport(Report):
     __name__ = 'school.payment.report'
+
+
